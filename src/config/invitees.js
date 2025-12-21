98f1c88a93ae22a163ce4bd4d92f1cbab6d8311b
Peter VandenKerckhove <peter.vandenkerckhove@ocular.be>
2025-12-21 10:50:43 +0100

    invitees update

--- src/config/invitees.js
+++ src/config/invitees.js
@@ -1,565 +1,168 @@
 import eventTypes from "./eventTypes";
 
-const familyXandrien = [
+const receptionInvitees = [
     {
-        event: eventTypes.all,
-        name: "Dirk Verthe",
-        partner: "Inge Roelstraete",
-    },
-    {
-        event: eventTypes.all,
-        name: "Lise Verthe",
-        partner: "Thomas Nys"
-    },
-    {
-        event: eventTypes.all,
-        name: "Kaat Roelstraete",
-        partner: "Frank Laverge",
-    },
-    {
-        event: eventTypes.all,
-        name: "Ria Roelstraete",
-        partner: "Bart De Leeuw"
-    },
-    {
-        event: eventTypes.all,
-        name: "Geert Roelstraete",
-        partner: "Marilene José"
-    },
-    {
-        event: eventTypes.all,
-        name: "Christine Verthé",
-        partner: "Johan Decoutere"
-    },
-    {
-        event: eventTypes.all,
-        name: "Frank Verthé",
-        partner: "Aimee Vandevoordt"
-    },
-    {
-        event: eventTypes.all,
-        name: "Marc Reynaert",
-        partner: "Ann Roelstraete"
-    },
-    {
-        event: eventTypes.all,
-        name: "Karel De Leeuw",
-        partner: "Lieve Vandevelde",
-        children: [
-            {
-                name: "Vic",
-            },
-            {
-                name: "Mila"
-            }
-        ]
-    },
-    {
-        event: eventTypes.all,
-        name: "Pieter-Jan De Leeuw",
-        partner: "Ann Baert",
-        children: [
-            {
-                name: "Lucas",
-            },
-            {
-                name: "Thomas"
-            }
-        ]
-    },
-    {
-        event: eventTypes.all,
-        name: "Rien Laverge",
-        partner: "Line Vyvey",
-        children: [
-            {
-                name: "Lotte",
-            },
-            {
-                name: "Julie"
-            }
-        ]
-    },
-    {
-        event: eventTypes.all,
-        name: "Jelle Laverge",
-        partner: "Sarah Vergote",
-        children: [
-            {
-                name: "Amber",
-            },
-            {
-                name: "Daan"
-            }
-        ]
-    },
-    {
-        event: eventTypes.all,
-        name: "Wim Roelstraete",
-        partner: "Mariana",
-        children: [
-            {
-                name: "Owen",
-            },
-        ]
-    },
-    {
-        event: eventTypes.all,
-        name: "Emmy Roelstraete",
-        partner: "Brian Verbeeck",
-        children: [
-            {
-                name: "Guillaume",
-            },
-            {
-                name: "Casper"
-            }
-        ]
-    },
-    {
-        event: eventTypes.all,
-        name: "Jan Verthé",
-        partner: "Sanne Croughs",
-        children: [
-            {
-                name: "Mien",
-            },
-            {
-                name: "Doet"
-            }
-        ]
-    },
-    {
-        event: eventTypes.all,
-        name: "Ben Verthé",
-        partner: "Sylvie Fabiano",
-        children: [
-            {
-                name: "Daria",
-            },
-            {
-                name: "Marlo"
-            }
-        ]
-    },
-    {
-        event: eventTypes.all,
-        name: "Tom Verthé",
-        partner: "Luana Russo",
-        children: [
-            {
-                name: "Vincent",
-            },
-            {
-                name: "Aurelie"
-            }
-        ]
-    },
-    {
-        event: eventTypes.all,
-        name: "Maaike Decoutere",
-        partner: "Davy Warnez",
-        children: [
-            {
-                name: "Abbygail",
-            },
-            {
-                name: "Ashley"
-            }
-        ]
-    },
-];
-
-const familyPeter = [
-    {
-        event: eventTypes.all,
-        name: "Guido VandenKerckhove",
-        partner: "Greta Delbaere",
-    },
-    {
-        event: eventTypes.all,
-        name: "Ellen VandenKerckhove",
-        partner: "Cedric Dekeyster",
-    },
-    {
-        event: eventTypes.all,
-        name: "Petra VandenKerckhove",
-        children: [
-            {
-                name: "Maura",
-            },
-        ]
-    },
-    {
-        event: eventTypes.all,
-        name: "Gerda VandenKerckhove",
-        partner: "Jozef Pype",
-    },
-    {
-        event: eventTypes.all,
-        name: "José Quiñones",
-    },
-    {
-        event: eventTypes.all,
-        name: "Tom Pype",
-        partner: "Amy Tanghe",
-        children: [
-            {
-                name: "Lotte",
-            },
-            {
-                name: "Ferre",
-            },
-        ]
-    },
-    {
-        event: eventTypes.all,
-        name: "Rita Delbaere",
-    },
-];
-
-const colleaguesXandrien = [
-    {
-        event: eventTypes.all,
-        name: "Victor Ducatelle",
-        partner: "Lobbe Denijs"
-    },
-    {
-        event: eventTypes.all,
-        name: "Kimberley Dumon",
-        partner: "Bert Cardoen"
-    },
-    {
-        event: eventTypes.all,
-        name: "Hannelore Degrendel",
-    },
-    {
-        event: eventTypes.all,
-        name: "Yves Durnez",
-        partner: "Katrien Dewitte"
-    },
-    {
-        event: eventTypes.all,
-        name: "Isabelle Bruneel",
-        partner: "Didier Decrock"
-    },
-    {
-        event: eventTypes.all,
-        name: "Eline Van Houtte",
-        partner: "Mathieu Staelens"
-    },
-    {
-        event: eventTypes.all,
-        name: "Mieke Desseyn",
-        partner: "Mohamed Demdoum"
-    },
-    {
-        event: eventTypes.all,
-        name: "Peggy Vanoverschelde",
-    },
-    {
-        event: eventTypes.all,
-        name: "Hannelore Defour",
-        partner: "Geert Janssens"
-    },
-    {
-        event: eventTypes.all,
-        name: "Karen Vanderhaeghe",
-        partner: "Bjorn Verbeest"
-    },
-    {
-        event: eventTypes.all,
-        name: "Ann Dewitte",
-        partner: "Nico Himpe"
-    },
-    {
-        event: eventTypes.all,
-        name: "Lien De Coster",
-        partner: "Tom Seyns"
-    },
-    {
-        event: eventTypes.all,
-        name: "Annelies Lust",
-        partner: "Anthony Luhning"
-    },
-    {
-        event: eventTypes.all,
-        name: "Riet Vanraes",
-        partner: "Bart Demunck"
-    },
-    {
-        event: eventTypes.all,
-        name: "Inge Vanwynsberghe",
-        partner: "Koen Vanneste"
-    },
-    {
-        event: eventTypes.all,
-        name: "Katrien Noyens",
-        partner: "Pieter Becquart"
-    },
-    {
-        event: eventTypes.all,
-        name: "Manon Noyez",
-        partner: "Pavel Dessein"
-    },
-    {
-        event: eventTypes.all,
-        name: "Silke De Pré",
-        partner: "Tom Clauw"
-    },
-    {
-        event: eventTypes.all,
-        name: "Mario Decrock",
-        partner: "Hanne Vercruysse"
-    },
-    {
-        event: eventTypes.all,
-        name: "Joyce Jacques",
-        partner: "Robbe Werniers"
-    },
-    {
-        event: eventTypes.all,
-        name: "Laurence Mareel",
+        event: eventTypes.reception,
+        name: "Carine Migneau",
     },
     {
         event: eventTypes.reception,
-        name: "Mieke De Koning",
+        name: "Johan Debal",
     },
     {
         event: eventTypes.reception,
-        name: "Ulrike Decuypere",
+        name: "Françoise Monard",
     },
     {
         event: eventTypes.reception,
-        name: "Corinne Depraetere",
+        name: "Jean Paul Rombeau",
     },
     {
         event: eventTypes.reception,
-        name: "Annelies Rosseel",
+        name: "Marleen",
     },
     {
         event: eventTypes.reception,
-        name: "Sien Accou",
+        name: "Patrick Houtekier",
     },
     {
         event: eventTypes.reception,
-        name: "Sabine Dupont",
+        name: "Veerle Verlinde",
     },
     {
         event: eventTypes.reception,
-        name: "Leen Desimpelaere",
+        name: "Andy Deseure",
     },
     {
         event: eventTypes.reception,
-        name: "Leen Dervaux",
+        name: "Marc Christiaens",
     },
     {
         event: eventTypes.reception,
-        name: "Fabienne Depoortere",
+        name: "Nathalie Tandt",
     },
     {
         event: eventTypes.reception,
-        name: "Sofie Schoonbaert",
+        name: "Mathias Christiaens",
     },
     {
         event: eventTypes.reception,
-        name: "Jill Nichelson",
+        name: "Aniana Lamon",
     },
     {
         event: eventTypes.reception,
-        name: "Heidi Declercq",
+        name: "Jonas Christiaens",
     },
     {
         event: eventTypes.reception,
-        name: "Lore Viaene",
+        name: "Isabo Van Fleteren",
     },
     {
         event: eventTypes.reception,
-        name: "Wouter Lannoy",
+        name: "Johanna Wylin",
     },
     {
         event: eventTypes.reception,
-        name: "Ann Callewaert",
+        name: "Jacques Abeel",
     },
     {
         event: eventTypes.reception,
-        name: "Lisa Vercruysse",
+        name: "Krista Wylin",
     },
     {
         event: eventTypes.reception,
-        name: "Nele Waignein",
-    },
-];
-
-const colleaguesPeter = [
-    {
-        event: eventTypes.all,
-        name: "Aaron Caenepeel",
+        name: "Johan Demasure",
     },
     {
-        event: eventTypes.all,
-        name: "Benedikt Beun",
+        event: eventTypes.reception,
+        name: "Greet Saelens",
     },
     {
-        event: eventTypes.all,
-        name: "Erik Boon",
+        event: eventTypes.reception,
+        name: "Filip Leenknegt",
     },
     {
-        event: eventTypes.all,
-        name: "Frederik Vermeiren",
+        event: eventTypes.reception,
+        name: "Sonja Declercq",
     },
     {
-        event: eventTypes.all,
-        name: "Els Vergison",
-    },
-    {
-        event: eventTypes.all,
-        name: "Hilde Doise",
-    },
-    {
-        event: eventTypes.all,
-        name: "Nele Vannobel",
-    },
-    {
-        event: eventTypes.all,
-        name: "Jo Tytgat",
-    },
-    {
-        event: eventTypes.all,
-        name: "Christophe Laloo",
-    },
-    {
-        event: eventTypes.all,
-        name: "Johan Vanoeteren",
+        event: eventTypes.reception,
+        name: "Didier Debonne",
     }
 ];
 
-const friends = [
-    {
-        event: eventTypes.all,
-        name: "Xenia Loosvelt",
-        partner: "Pieter Deprez"
-    },
-    {
-        event: eventTypes.all,
-        name: "Kayleigh Robson",
-        partner: "Louis Demuynck"
-    },
-    {
-        event: eventTypes.all,
-        name: "Kelly Louagie",
-        partner: "Linda Goethals"
-    },
-    {
-        event: eventTypes.all,
-        name: "Nikita Wyllemet",
-        partner: "Thomas De Loof"
-    },
-    {
-        event: eventTypes.all,
-        name: "Evelyne Taillieu",
-        partner: "Nicolas Vandekerckhove"
-    },
-    {
-        event: eventTypes.all,
-        name: "Vincent Taillieu",
-    },
-    {
-        event: eventTypes.all,
-        name: "Tim Libbrecht",
-    },
-    {
-        event: eventTypes.all,
-        name: "Melissa Byttebier",
-    },
-    {
-        event: eventTypes.all,
-        name: "Natasja Maes",
-        partner: "Thomas Engerie"
-    },
-    {
-        event: eventTypes.all,
-        name: "Céline Vandevyvere",
-    },
-    {
-        event: eventTypes.all,
-        name: "Bram Swyngedauw",
-    },
-    {
-        event: eventTypes.all,
-        name: "Jeroen Thibau",
-        partner: "Emily Roels"
-    },
-    {
-        event: eventTypes.all,
-        name: "Jana Ossieur",
-        partner: "Michiel Demeyere"
-    },
-    {
-        event: eventTypes.all,
-        name: "Sven Demeyere",
-        partner: "Angie Delbeke"
-    },
-    {
-        event: eventTypes.all,
-        name: "Chris Swyngedauw",
-    },
-    {
-        event: eventTypes.all,
-        name: "Geert Swyngedauw",
-        partner: "Katrien Vandamme"
-    },
-    {
-        event: eventTypes.all,
-        name: "Niels De Witte",
-        partner: "Amber Destrijcker"
-    },
-    {
-        event: eventTypes.all,
-        name: "Marieke Vanbelle",
-        partner: "Brent Modde"
-    },
-]
+const dinerInvitees = [
+    { event: eventTypes.all, name: "Elsy Claeys" },
+    { event: eventTypes.all, name: "Krist Mylle" },
+    { event: eventTypes.all, name: "Wout Mylle" },
+    { event: eventTypes.all, name: "Stephanie Delameilleure" },
+    { event: eventTypes.all, name: "Heiko Martin" },
+    { event: eventTypes.all, name: "Barbara Desmet" },
+    { event: eventTypes.all, name: "Guillaume Mareel" },
+    { event: eventTypes.all, name: "Victoria Martin" },
+    { event: eventTypes.all, name: "Rita Wylin" },
+    { event: eventTypes.all, name: "Ria Quaghebeur" },
+    { event: eventTypes.all, name: "Luc Piepers" },
+    { event: eventTypes.all, name: "Liliane Desmedt" },
+    { event: eventTypes.all, name: "Sabine Mylle" },
+    { event: eventTypes.all, name: "Rudy Vanhaelewyn" },
+    { event: eventTypes.all, name: "Manon Vanhaelewyn" },
+    { event: eventTypes.all, name: "Jules Vandamme" },
+    { event: eventTypes.all, name: "Reinout Vanhaelewyn" },
+    { event: eventTypes.all, name: "Ellen Cools" },
+    { event: eventTypes.all, name: "Marie Vanhaelewyn" },
+    { event: eventTypes.all, name: "Tibo Strubbe" },
+    { event: eventTypes.all, name: "Patsy Grimonprez" },
+    { event: eventTypes.all, name: "Bart Mylle" },
+    { event: eventTypes.all, name: "Alexander Mylle" },
+    { event: eventTypes.all, name: "Jonathan Mylle" },
+    { event: eventTypes.all, name: "Imani Tanghe" },
+    { event: eventTypes.all, name: "Anniek Gryp" },
+    { event: eventTypes.all, name: "Andy Claeys" },
+    { event: eventTypes.all, name: "Zoë Claeys" },
+    { event: eventTypes.all, name: "Uwe Claeys" },
+    { event: eventTypes.all, name: "Johan Claeys" },
+    { event: eventTypes.all, name: "Sandy Lannoo" },
+    { event: eventTypes.all, name: "Ellen Claeys" },
+    { event: eventTypes.all, name: "Sharon Claeys" },
+    { event: eventTypes.all, name: "jamien Tack" },
+    { event: eventTypes.all, name: "Els Desmet" },
+    { event: eventTypes.all, name: "Vincent Lampole" },
+    { event: eventTypes.all, name: "Manou Devogelaere" },
+    { event: eventTypes.all, name: "Leon Devogelaere" },
+    { event: eventTypes.all, name: "Viktor Vanden Berghe" },
+    { event: eventTypes.all, name: "Babette Pauwelyn" },
+    { event: eventTypes.all, name: "Helene Gruyaert" },
+    { event: eventTypes.all, name: "Michiel Caenepeel" },
+    { event: eventTypes.all, name: "Hugo Gentenaar" },
+    { event: eventTypes.all, name: "Jade Christiaens" },
+    { event: eventTypes.all, name: "Rune Grimmelprez" },
+    { event: eventTypes.all, name: "Toon Van Hove" },
+    { event: eventTypes.all, name: "Kato Meeuws" },
+    { event: eventTypes.all, name: "Meredith Huyghebaert" },
+    { event: eventTypes.all, name: "Marijn Vervaecke" },
+    { event: eventTypes.all, name: "Xandrien Verthe", partner: "Peter VandenKerckhove" },
+    { event: eventTypes.all, name: "Victor Ducatelle", partner: "Lobbe Denijs" },
+    { event: eventTypes.all, name: "Gianni Vullers" },
+    { event: eventTypes.all, name: "Alana Popelier" },
+    { event: eventTypes.all, name: "Pieter Lammens" },
+    { event: eventTypes.all, name: "Ine Neerinck" },
+    { event: eventTypes.all, name: "Xanthippe Maes" },
+    { event: eventTypes.all, name: "Laura Vandamme" },
+    { event: eventTypes.all, name: "Jonas Christiaens" },
+    { event: eventTypes.all, name: "Romy Lenoor" },
+    { event: eventTypes.all, name: "Aïsha Miya" },
+    { event: eventTypes.all, name: "Alex Vanlauwe" },
+    { event: eventTypes.all, name: "Leen Decroos" },
+    { event: eventTypes.all, name: "Emile Tytgat" },
+    { event: eventTypes.all, name: "Korneel Decroos" },
+    { event: eventTypes.all, name: "Marlies Lievens" },
+    { event: eventTypes.all, name: "Fabien Francken" }
+];
 
 const invitees = [
-    ...familyXandrien,
-    ...familyPeter,
-    ...colleaguesXandrien,
-    ...colleaguesPeter,
-    ...friends,
-    {
-        event: eventTypes.reception,
-        name: "Jari Goemaere",
-    },
-    {
-        event: eventTypes.reception,
-        name: "Philippe Delmotte",
-    },
-    {
-        event: eventTypes.reception,
-        name: "Rino Casier",
-    },
-    {
-        event: eventTypes.reception,
-        name: "Arne Vandorpe",
-    },
-    {
-        event: eventTypes.reception,
-        name: "Yoeri Vermeersch",
-    },
-    {
-        event: eventTypes.reception,
-        name: "Jan Taillieu",
-        partner: "Nathalie Deleu"
-    },
-    {
-        event: eventTypes.reception,
-        name: "Myriam Dirix",
-    },
-    {
-        event: eventTypes.all,
-        name: "Koen Beys",
-    },
+    ...receptionInvitees,
+    ...dinerInvitees
 ]
 
 export default invitees;
